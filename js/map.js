18344f14db9a73274716f76aacb1e4bef7947a4d
Can Chen <canchen@me.com>
2017-11-08 12:07:12 -0500

    feat: add header, includes a search bar to find locations in google map
    header search bar using `google.maps.places.SearchBox` api.
    user now able to find locations on map use search bar, upon submitting query, map will center at desired location.

--- js/map.js
+++ js/map.js
@@ -10,4 +10,58 @@ function initMap() {
       position: google.maps.ControlPosition.TOP_RIGHT
     },
   })
+
+  // Search bar for locations.
+  // https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+  const input = document.getElementById('mapSearch')
+  const searchBox = new google.maps.places.SearchBox(input)
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds())
+  })
+
+  let markers = []
+  searchBox.addListener('places_changed', function() {
+    let places = searchBox.getPlaces()
+    if(places.length == 0) {
+      return
+    }
+
+    markers.forEach(function(marker) {
+      marker.setMap(null)
+    })
+    markers = []
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds()
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry")
+        return
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      }
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }))
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport)
+      } else {
+        bounds.extend(place.geometry.location)
+      }
+    })
+    map.fitBounds(bounds)
+  })
 }
