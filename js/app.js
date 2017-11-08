17c4834368eafb44385f77c616c535d5bee37493
Can Chen <canchen@me.com>
2017-11-08 12:09:01 -0500

    feat: add side menu, where display saved places, user filter places.
    Mock up version of it right now, implementations on the way

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 function AppViewModel() {
+  this.location = ko.observable('')
+  this.ph = 'Enter Location'
+  this.filters = ko.observableArray(['1', '2', '3', '4'])
 
+  this.address = ko.observableArray([
+    {address: '101 cormack circle'},
+    {address: '202 go north way'},
+    {address: '303 hungury road'},
+    {address: '404 not found'},
+    {address: '505 in the air'}
+  ])
+
+  this.handleClick = function(target) {
+    console.log(target)
+  }
 }
 
 ko.applyBindings(new AppViewModel())
